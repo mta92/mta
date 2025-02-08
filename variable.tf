a96ded07f16fd0c5b99a81ee67281d62c2e987ec
mta92 <95572494+mta92@users.noreply.github.com>
2025-02-07 19:10:44 -0500

    Create variable.tf

---
+++ variable.tf
@@ -0,0 +1,18 @@
+
+variable "subscription_id"{
+  type=string
+}
+
+variable "client_id"{
+  type=string
+}
+variable "client_secret"{
+  type=string
+}
+variable "tenant_id"{
+  type=string
+}
+variable "account_tier"{
+  type=string
+ default="Premium"
+}
