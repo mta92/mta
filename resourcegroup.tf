0b221099b9bef9ac0a691f907b3a89a2aabec9ad
mta92 <95572494+mta92@users.noreply.github.com>
2025-02-07 19:08:55 -0500

    Create resourcegroup.tf

---
+++ resourcegroup.tf
@@ -0,0 +1,16 @@
+resource "azurerm_resource_group" "mcit420zz5um" {
+  name     = "mcit_resource_group_w24"
+  location = "canadacentral"
+}
+resource "azurerm_resource_group" "mcit420zz5um2" {
+  name     = "mcit_resource_group_w24_2"
+  location = "canadacentral"
+}
+resource "azurerm_resource_group" "mcit420zz5um3" {
+  name     = "mcit_resource_group_w24_3"
+  location = "canadacentral"
+}
+resource "azurerm_resource_group" "mcit420zz5um4" {
+  name     = "mcit_resource_group_w24_4"
+  location = "canadacentral"
+}
